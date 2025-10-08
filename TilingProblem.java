6d1c5c1bad71fcb4c7e34678f9867207fdfe6d6f
Dipan <roydipan05@gmail.com>
2025-10-08 20:37:14 +0530

    explanations

--- TilingProblem.java
+++ TilingProblem.java
@@ -1,5 +1,11 @@
+// Tiling Problem :-
+// Given a "2 x n" board and tiles of size "2 x 1", 
+// count the number of ways to tile the given board using the 2 x 1 tiles.
+// (A tile can either be placed horizontally or vertically. )
+
 public class TilingProblem {
     public static int tilingWays(int n){ //2 x n (floor size)
+        // Base case
         if(n == 1 || n == 0){
             return 1;
         }
